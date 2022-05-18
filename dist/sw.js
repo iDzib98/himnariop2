39eb804f04203e109de961cb843cc281cea4878e
iDzib98 <is-toni@live.com.mx>
2022-05-18 17:37:02 -0500

    Se añade dist al github

---
+++ dist/sw.js
@@ -0,0 +1,71 @@
+var cacheName = "himnario_v1";
+var filesToCache = [
+  './assets/color-circles/amber.svg',
+  './assets/color-circles/blue-grey.svg',
+  './assets/color-circles/blue.svg',
+  './assets/color-circles/brown.svg',
+  './assets/color-circles/cyan.svg',
+  './assets/color-circles/deep-orange.svg',
+  './assets/color-circles/deep-purple.svg',
+  './assets/color-circles/green.svg',
+  './assets/color-circles/grey.svg',
+  './assets/color-circles/indigo.svg',
+  './assets/color-circles/light-blue.svg',
+  './assets/color-circles/light-green.svg',
+  './scripts/materialize.min.js',
+  './styles/global.css',
+  './styles/materialize.min.css',
+  './styles/revealjs/black.css',
+  './styles/revealjs/reveal.css',
+  './styles/revealjs/white.css'
+];
+
+var himnosCache = "himnos_v1";
+var files_Himnos = [
+  '.',
+  './index.html',
+  './main.js'
+];
+
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+  e.waitUntil(
+    caches.open(himnosCache).then(function(cache) {
+      console.log('[ServiceWorker] Caching HimnosOffline');
+      return cache.addAll(files_Himnos);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== himnosCache && key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('[Service Worker] Fetch', e.request.url);
+  
+      e.respondWith(
+        caches.match(e.request).then(function(response) {
+          return response || fetch(e.request);
+        })
+      );
+  });
+  
